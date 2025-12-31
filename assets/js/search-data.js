44b9190273d88187981cee6ce0b853291030a7a7
rmsouza01 <roberto.medeiros.souza@gmail.com>
2025-12-31 06:13:47 +0000

    Deploying to gh-pages from @ uofc-ai2lab/uofc-ai2lab.github.io@ba80aef6eaaceeb200c52d81913594e453ed6359 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-directors",
-          title: "directors",
+          title: "Directors",
           description: "Directors of the AI2Lab.",
           section: "Navigation",
           handler: () => {
