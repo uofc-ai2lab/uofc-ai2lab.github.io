432c46b5f96059f6f0d04be92c57e5e923d65e6f
lab-ai2 <anik.das@ucalgary.ca>
2026-01-26 09:50:10 +0000

    Deploying to gh-pages from @ uofc-ai2lab/uofc-ai2lab.github.io@3d836ca77c15840df5b7dd11440411771b5398a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,8 +452,8 @@ ninja.data = [{
           description: "PhD Candidate - BME",
           section: "People",handler: () => {
               window.location.href = "/people/Tasneem_Nasser.html";
-            },},{id: "projects-project-1-imri-integrated-magnetic-resonance-imaging",
-          title: 'Project 1 IMRI: Integrated Magnetic Resonance Imaging',
+            },},{id: "projects-project-1",
+          title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
