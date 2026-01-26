98497d7d92faf5178e689e785772b69c0ba10faa
lab-ai2 <anik.das@ucalgary.ca>
2026-01-26 12:29:25 +0000

    Deploying to gh-pages from @ uofc-ai2lab/uofc-ai2lab.github.io@ddf2ea368422eab166b808e39a6ebfbc26aac1b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-projects",
+          title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
