df77386dc69dc214db1a434651516dbebd1a3e74
lab-ai2 <anik.das@ucalgary.ca>
2026-09-22 16:05:17 +0000

    Deploying to gh-pages from @ uofc-ai2lab/uofc-ai2lab.github.io@256e0df7ecfead4b5140a9cc21897be6a16d6320 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-alumni",
           title: "Alumni",
-          description: "Former members of the AI2Lab.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/alumni/";
